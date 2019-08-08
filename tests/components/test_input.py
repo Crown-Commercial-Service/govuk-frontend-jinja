1fea329edf02521620623089877e7cde8e1453d1
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-08-08 17:27:27 +0100

    Add tests for input component

--- tests/components/test_input.py
+++ tests/components/test_input.py
@@ -1,4 +1,6 @@
 
+import pytest
+
 from govuk_frontend.templates import Environment
 
 
@@ -29,4 +31,89 @@ def test_input():
 
   <input class="govuk-input" id="input-example" name="test-name" type="text">
 </div>"""
+    )
+
+
+def test_input_with_hint_text():
+    env = Environment()
+    template = env.from_string(
+r"""
+{% from "input/macro.njk" import govukInput %}
+
+{{ govukInput({
+  "label": {
+    "text": "National insurance number"
+  },
+  "hint": {
+    "text": "It’s on your National Insurance card, benefit letter, payslip or P60\. For example, ‘QQ 12 34 56 C’."
+  },
+  "id": "input-with-hint-text",
+  "name": "test-name-2"
+}) }}
+"""
+    )
+    assert (
+        pytest.helpers.normalise_whitespace(template.render())
+        ==
+        pytest.helpers.normalise_whitespace(
+r"""
+<div class="govuk-form-group">
+  <label class="govuk-label" for="input-with-hint-text">
+    National insurance number
+  </label>
+
+  <span id="input-with-hint-text-hint" class="govuk-hint">
+    It’s on your National Insurance card, benefit letter, payslip or P60\. For example, ‘QQ 12 34 56 C’.
+  </span>
+
+  <input class="govuk-input" id="input-with-hint-text" name="test-name-2" type="text" aria-describedby="input-with-hint-text-hint">
+</div>
+"""
+        )
+    )
+
+
+def test_input_with_error_mesage():
+    env = Environment()
+    template = env.from_string(
+r"""
+{% from "input/macro.njk" import govukInput %}
+
+{{ govukInput({
+  "label": {
+    "text": "National Insurance number"
+  },
+  "hint": {
+    "text": "It’s on your National Insurance card, benefit letter, payslip or P60\. For example, ‘QQ 12 34 56 C’."
+  },
+  "id": "input-with-error-message",
+  "name": "test-name-3",
+  "errorMessage": {
+    "text": "Error message goes here"
+  }
+}) }}
+"""
+    )
+    assert (
+        pytest.helpers.normalise_whitespace(template.render())
+        ==
+        pytest.helpers.normalise_whitespace(
+            r"""
+            <div class="govuk-form-group govuk-form-group--error">
+              <label class="govuk-label" for="input-with-error-message">
+                National Insurance number
+              </label>
+
+              <span id="input-with-error-message-hint" class="govuk-hint">
+                It’s on your National Insurance card, benefit letter, payslip or P60\. For example, ‘QQ 12 34 56 C’.
+              </span>
+
+              <span id="input-with-error-message-error" class="govuk-error-message">
+                Error message goes here
+              </span>
+
+              <input class="govuk-input govuk-input--error" id="input-with-error-message" name="test-name-3" type="text" aria-describedby="input-with-error-message-hint input-with-error-message-error">
+            </div>
+            """
+        )
     )
