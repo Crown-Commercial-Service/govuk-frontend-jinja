18bcbb813bdd8aacdb3cd865eb3f1f316c7af388
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-08-08 10:26:48 +0100

    Remove template_file fixture

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -11,19 +11,15 @@ def environment():
     return mock.Mock()
 
 
-@pytest.fixture(autouse=True)
-def template_file():
-    mock_open = mock.mock_open(read_data=b"foobar")
-    with mock.patch("jinja2.utils.open", mock_open):
-        yield mock_open
-
-
 @pytest.fixture
 def loader():
     return NunjucksFileSystemLoader("node_modules/govuk-frontend")
 
 
-def test_gets_source_from_filesystem(environment, template_file, loader):
-    contents, _, _ = loader.get_source(environment, "template.njk")
-    assert contents == "foobar"
-    assert template_file.call_count == 1
+def test_gets_source_from_filesystem(environment, loader):
+    mock_open = mock.mock_open(read_data=b"foobar")
+    with mock.patch("jinja2.utils.open", mock_open) as m:
+        contents, _, _ = loader.get_source(environment, "template.njk")
+
+        assert contents == "foobar"
+        assert m.call_count == 1
