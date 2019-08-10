c291d65f9161f37f0cbfc18c263d61e25aa7bf9e
Laurence de Bruxelles <lfdebrux@gmail.com>
2019-08-10 01:13:43 +0100

    Add NunjucksExtension

--- govuk_frontend/templates.py
+++ govuk_frontend/templates.py
@@ -1,5 +1,6 @@
 
 import jinja2
+import jinja2.ext
 
 import os.path as path
 
@@ -8,10 +9,19 @@ def njk_to_j2(template):
     return template
 
 
+class NunjucksExtension(jinja2.ext.Extension):
+    def preprocess(self, source, name, filename=None):
+        if filename and filename.endswith(".njk"):
+            return njk_to_j2(source)
+        else:
+            return source
+
+
 class Environment(jinja2.Environment):
     def __init__(self, **kwargs):
+        kwargs.setdefault("extensions", [NunjucksExtension])
         kwargs.setdefault("loader",
-            jinja2.loaders.FileSystemLoader([
+            jinja2.FileSystemLoader([
                 "node_modules/govuk-frontend",
                 "node_modules/govuk-frontend/components",
         ]))
