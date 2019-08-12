32cd4b186b38b46f5fa9f9330b6cfc1c72d07cc3
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-08-12 09:06:56 +0100

    Add script to get govuk-frontend package from GitHub

---
+++ scripts/get-govuk-frontend.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Download the govuk-frontend package with component templates for testing.
+
+VERSION="2.4.0"
+URL="https://github.com/alphagov/govuk-frontend/archive/v${VERSION}.tar.gz"
+SHA256="a8be5ab794aefa906e3710db5ae3520da800182985cd2d44397ab1cbe82544b9"
+
+set -e
+
+tarball="govuk-frontend-${VERSION}.tar.gz"
+
+# download
+[ -f "$tarball" ] || curl -OJL "$URL" --fail
+
+# check
+echo "$SHA256 $tarball" | sha256sum --check --quiet
+
+# extract
+mkdir -p node_modules/govuk-frontend
+tar -xf $tarball --skip-old-files -C node_modules/govuk-frontend --strip-components=2 "govuk-frontend-${VERSION}/package/"
