0b33eb2d2d34032b09a143d388e5f3ae3eca5c43
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-08-14 15:47:26 +0100

    Fix bug with `join_path`
    `join_path` was trying to join paths even when the path wasn't relative
    (i.e. didn't start with `./`). This commit fixes this and adds tests.

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -11,6 +11,31 @@ def test_environment_contains_govuk_frontend_templates_which_can_be_rendered():
     assert template.render()
 
 
+class TestJoinPath:
+
+    @pytest.fixture
+    def env(self):
+        return Environment(
+            loader=jinja2.DictLoader({
+                "a/a/a.njk": "{% include './b.njk' %}",
+                "a/a/b.njk": "b",
+                "a/b/b.njk": "{% include '../a/b.njk' %}",
+                "c/c.html": "{% include 'a/a/a.njk' %}",
+            })
+        )
+
+    def test_join_path_joins_relative_paths(self, env):
+        template = env.get_template("a/a/a.njk")
+        assert template.render() == "b"
+
+        template = env.get_template("a/b/b.njk")
+        assert template.render() == "b"
+
+    def test_join_path_does_not_join_unanchored_paths(self, env):
+        template = env.get_template("c/c.html")
+        assert template.render() == "b"
+
+
 class TestNunjucksUndefined:
     
     @pytest.fixture
