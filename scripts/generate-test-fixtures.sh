b6afec4aab6c8e1b1dd0788330d72510258e2565
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-09-24 16:20:30 +0100

    Add a script to generate expected output from Nunjucks

---
+++ scripts/generate-test-fixtures.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Generate the expected output for all test fixture using Nunjucks
+
+if ! command -v nunjucks >/dev/null 2>&1
+then
+	2>&1 echo "error: this script requires nunjucks-cli but it is not installed"
+	2>&1 echo
+	2>&1 echo "Please ensure that Node and npm are installed and then run:"
+	2>&1 echo "    npm install -g github:lfdebrux/nunjucks-cli#dev"
+	exit 1
+fi
+
+nunjucks \
+	--path tests/components \
+	--path node_modules/govuk-frontend \
+	--path node_modules/govuk-frontend/components \
+	--out tests/components \
+	--original-extension .t.html \
+	--extension x.html \
+	'**/*.t.html'
