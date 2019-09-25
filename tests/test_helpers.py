fb2e327e8c08b776a2f416ff6b0c24ebd8672d8f
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-09-25 10:25:36 +0100

    Add check on whether user is testing correct version of govuk-frontend

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,5 +1,7 @@
-
 import pytest
+
+import warnings
+
 
 def test_normalise_whitespace_deletes_empty_lines():
     assert (
@@ -32,4 +34,16 @@ def test_normalise_whitespace_can_be_imported_with_another_name():
 
 
 def test_govuk_frontend_version():
-    assert pytest.helpers.govuk_frontend_version_info() == (2, 13, 0)
+    version_info = pytest.helpers.govuk_frontend_version_info()
+    # assert check_type(version_info, Tuple[int, int, int])
+    assert isinstance(version_info, tuple)
+    assert all(isinstance(i, int) for i in version_info)
+
+    def warn_if_not_matching_version(want, got):
+        if want != got:
+            warnings.warn(
+                f"Found govuk-frontend version {got}, expected {want} - some tests may fail because of this! To ensure you have the correct version downloaded run ./scripts/get-govuk-frontend.sh",
+                stacklevel=2,
+            )
+
+    warn_if_not_matching_version((2, 13, 0), version_info)
