429f5dac0638eafba05b0b75286a258ffcf94b16
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-10-25 11:52:30 +0100

    Simplify tests for flask_ext

--- tests/test_flask_ext.py
+++ tests/test_flask_ext.py
@@ -7,17 +7,20 @@ import govuk_frontend_jinja
 from govuk_frontend_jinja.flask_ext import Environment, init_govuk_frontend
 
 @pytest.fixture
-def app():
-    return flask.Flask("test_flask_ext")
+def app(loader):
+    app = flask.Flask("test_flask_ext")
+    init_govuk_frontend(app)
+    app.jinja_loader = loader
+    return app
 
 
-def test_environment_takes_app_as_first_argument(app):
+def test_environment_takes_app_as_first_argument():
+    app = flask.Flask("test_flask_ext")
     env = Environment(app)
     assert env.app == app
 
 
 def test_init_govuk_frontend(app):
-    init_govuk_frontend(app)
     env = app.jinja_env
 
     assert (
@@ -32,9 +35,6 @@ def test_init_govuk_frontend(app):
     )
 
 
-def test_render_template(app, loader):
-    init_govuk_frontend(app)
-    app.jinja_loader = loader
-
+def test_render_template(app):
     with app.app_context():
         flask.render_template("template.njk")
