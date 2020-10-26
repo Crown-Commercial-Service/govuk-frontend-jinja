1eaa3bafa27a36a6e417a7eea83999ff248d2d92
BRETT KYLE <brett.kyle@digital.cabinet-office.gov.uk>
2020-10-26 10:19:07 +0000

    Add test radios with conditional

--- tests/components/radios/test_radios.py
+++ tests/components/radios/test_radios.py
@@ -23,6 +23,11 @@ def test_radios_with_a_divider(env, similar, template, expected):
     assert similar(template.render(), expected)
 
 
+def test_radios_with_a_conditional(env, similar, template, expected):
+    template = env.from_string(template)
+    assert similar(template.render(), expected)
+
+
 def test_radios_with_hints_on_items(env, similar, template, expected):
     template = env.from_string(template)
     assert similar(template.render(), expected)
